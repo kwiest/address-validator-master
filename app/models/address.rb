4892467c8b8a148bf352f925a3cfc6e35e137d50
Kyle Wiest <kyle.wiest@gmail.com>
2018-07-10 20:31:40 -0700

    Add Address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,10 @@
 class Address < ApplicationRecord
+  validates :house_number, presence: true
+  validates :street_name, presence: true
+  validates :city, presence: true
+  validates :state, presence: true, length: { is: 2 }
+  validates :zip_5, presence: true, numericality: true, length: { is: 5 }
+
   def to_s
     # TODO: override the to_s method so that it prints out the address components as follows
     # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,40 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Address, :type => :model do
-  describe '#new' do
-    context 'Given a valid address' do
-      it 'can create a new address' do
-        expect(Address.new(
-                          house_number: 1600,
-                          street_name: 'Pennsylvania',
-                          street_type: 'Avenue',
-                          street_postdirection: 'NW',
-                          city: 'Washington',
-                          state: 'DC',
-                          zip_5: 20500
-                        )).to be_valid
-      end
-    end
+  it { is_expected.to validate_presence_of :house_number }
+  it { is_expected.to validate_presence_of :street_name }
+  it { is_expected.to validate_presence_of :city }
+  it { is_expected.to validate_presence_of :state }
+  it { is_expected.to validate_length_of(:state).is_equal_to(2) }
+  it { is_expected.to validate_presence_of :zip_5 }
+  it { is_expected.to validate_length_of(:zip_5).is_equal_to(5) }
+  it { is_expected.to validate_numericality_of :zip_5 }
 
-    context 'Given bad address values' do
-      it 'cannot create a new address' do
-        expect(Address.new(
-                          house_number: 1600,
-                          street_name: 'Pennsylvania',
-                          street_type: 'Avenue',
-                          street_postdirection: 'NW',
-                          city: 'Washington',
-                          state: 'DC',
-                          zip_5: 123
-                        )).not_to be_valid
-      end
-    end
-
-    describe '#to_s' do
-      let(:address) { create(:address_ny) }
-      it 'prints out the address components needed for mailing together as a string' do
-        expect(address.to_s).to eq('129 W 81st St Apt 5A, New York, NY 10024')
-      end
+  describe '#to_s' do
+    let(:address) { create(:address_ny) }
+    it 'prints out the address components needed for mailing together as a string' do
+      expect(address.to_s).to eq('129 W 81st St Apt 5A, New York, NY 10024')
     end
   end
 end
