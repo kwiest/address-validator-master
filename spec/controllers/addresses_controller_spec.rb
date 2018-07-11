5f95baa2435e109a173a3d3bdde997ab00efedff
Kyle Wiest <kyle.wiest@gmail.com>
2018-07-10 22:41:14 -0700

    Update controller specs

--- spec/controllers/addresses_controller_spec.rb
+++ spec/controllers/addresses_controller_spec.rb
@@ -5,41 +5,27 @@ RSpec.describe AddressesController, :type => :controller do
     context 'with valid params' do
       let(:valid_params) {
         {
-          street_address: '1600 Pennsylvania Avenue NW',
-          city: 'Washington',
-          state: 'DC',
-          zip_code: '20500'
+          address_verification: {
+            street_address: '1600 Pennsylvania Avenue NW',
+            city: 'Washington',
+            state: 'DC',
+            zip_code: '20500'
+          }
         }
       }
-      before do
-        post 'create', params: valid_params
-      end
-      it 'creates a new address model with the house number saved' do
-        expect(assigns(:address).house_number).to eq '1600'
-      end
 
-      it 'creates a new address model with the street name saved' do
-        expect(assigns(:address).street_name).to eq 'Pennsylvania'
-      end
-
-      it 'creates a new address model with the street name saved' do
-        expect(assigns(:address).street_type).to eq 'Avenue'
-      end
-
-      it 'creates a new address model with the street post direction saved' do
-        expect(assigns(:address).street_postdirection).to eq 'NW'
-      end
-
-      it 'creates a new address model with the city saved' do
-        expect(assigns(:address).city).to eq 'Washington'
-      end
-
-      it 'creates a new address model with the state saved' do
-        expect(assigns(:address).state).to eq 'DC'
-      end
-
-      it 'creates a new address model with the zip_5 saved' do
-        expect(assigns(:address).zip_5).to eq '20500'
+      it 'creates a new address' do
+        VCR.use_cassette :lob_deliverable_verification do
+          post 'create', params: valid_params
+          expect(assigns(:address).house_number).to eq('1709')
+          expect(assigns(:address).street_name).to eq('BRODERICK')
+          expect(assigns(:address).street_type).to eq('ST')
+          expect(assigns(:address).city).to eq('SAN FRANCISCO')
+          expect(assigns(:address).state).to eq('CA')
+          expect(assigns(:address).county).to eq('SAN FRANCISCO')
+          expect(assigns(:address).zip_5).to eq('94115')
+          expect(assigns(:address).zip_4).to eq('2525')
+        end
       end
     end
   end
