56776ca73d001375147e00d05f27f60789e2bf04
Kyle Wiest <kyle.wiest@contractor.cengage.com>
2018-07-11 10:17:29 -0700

    Use a select input for address state

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,9 @@
 module AddressesHelper
+  def state_abbreviations
+    %w(
+      AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI
+      MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA PR RI SC SD TN TX UT
+      VA VT VI WA WI WY
+    )
+  end
 end
