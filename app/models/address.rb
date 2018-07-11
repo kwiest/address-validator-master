f77cd7dc8f4f683b622543362596992f2240c351
Kyle Wiest <kyle.wiest@gmail.com>
2018-07-10 20:45:02 -0700

    Implement Address#to_s

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,7 +6,12 @@ class Address < ApplicationRecord
   validates :zip_5, presence: true, numericality: true, length: { is: 5 }
 
   def to_s
-    # TODO: override the to_s method so that it prints out the address components as follows
-    # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
+    street = [
+      house_number, street_predirection, street_name, street_type,
+      street_postdirection, unit_type, unit_number
+    ].reject(&:blank?).join(' ')
+    state_zip = [ state, zip_5 ].join(' ')
+
+    [ street, city, state_zip ].join(', ')
   end
 end
